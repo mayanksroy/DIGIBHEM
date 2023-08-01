8a87d1cdd2f8b7afc45aac88f9985e6835767c9b
mayanksroy <mayanksingh200241@gmail.com>
2023-08-02 01:20:04 +0530

    modified code

--- TEXT_TO_SPEECH/main.py
+++ TEXT_TO_SPEECH/main.py
@@ -1,6 +1,17 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import filedialog
-from tkinter import ComboBox
+from tkinter.ttk import Combobox
 import pyttsx3
 import os
+
+root=Tk()
+root.title("Text To Speech App using Python")
+root.geometry("900x450")
+root.resizable(False,False)
+root.config
+
+image_icon=PhotoImage(file="")
+root.iconphoto(False,image_icon)
+
+root.mainloop()
