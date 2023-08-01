dbed3f443b99dd83fa31f0a13fc7e7a203850b66
mayanksroy <mayanksingh200241@gmail.com>
2023-08-02 02:38:33 +0530

    changes made

--- TEXT_TO_SPEECH/main.py
+++ TEXT_TO_SPEECH/main.py
@@ -11,6 +11,13 @@ root.geometry("900x450")
 root.resizable(False,False)
 root.config(bg="#999999")
 
+image_icon = PhotoImage(file=r"TEXT_TO_SPEECH\icon.png")
+root.iconphoto(False, image_icon)
 
+Top_frame = Frame(root, bg="white", width=900, height=115)
+Top_frame.place(x=0, y=0)
+
+Logo=PhotoImage(file=r"TEXT_TO_SPEECH\icon1.png")
+Label(Top_frame, image=Logo, bg="white").place(x=20, y=5)
 
 root.mainloop()
